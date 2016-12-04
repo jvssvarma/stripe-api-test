14f181d0a9cc4482938a836721138cec337cb216
jvssvarma <varma.santosh@outlook.com>
2016-12-03 19:05:07 -0800

    fix aws region in initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ if Rails.env.production?
       :provider => 'AWS',
       :aws_access_key_id => ENV['AWS_ACCESS_KEY'],
       :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :aws_region => ENV['AWS_REGION']
+      :region => ENV['AWS_REGION']
     }
     config.fog_directory = ENV['AWS_S3_BUCKET']
   end
