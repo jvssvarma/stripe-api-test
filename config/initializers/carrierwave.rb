029cffa6a8deaef6e4d990334245d49dbcd11579
jvssvarma <varma.santosh@outlook.com>
2016-12-03 19:02:06 -0800

    Add Image uploading with production storage to S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ if Rails.env.production?
     config.fog_credentials = {
       :provider => 'AWS',
       :aws_access_key_id => ENV['AWS_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['AWS_SECRET_KEY'],
+      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
       :aws_region => ENV['AWS_REGION']
     }
     config.fog_directory = ENV['AWS_S3_BUCKET']
